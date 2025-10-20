ea5664fde05731ad0d6168c9f5b028cb20304ad7
doowoncho <84430881+doowoncho@users.noreply.github.com>
2025-10-19 22:30:53 -0600

    complete rehaul of site

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const menu = document.getElementById("menu");
+// const menu = document.getElementById("menu");
 
-Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.onmouseover = () => {
-      menu.dataset.activeIndex = index;
-    }
-  });
+// Array.from(document.getElementsByClassName("menu-item"))
+//   .forEach((item, index) => {
+//     item.onmouseover = () => {
+//       menu.dataset.activeIndex = index;
+//     }
+//   });
